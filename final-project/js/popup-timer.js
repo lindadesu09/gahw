6cc7d6d1c5f3659e2c93c45d0f72ebb309d1e852
Linda Chan <lindadesu09@gmail.com>
2014-09-27 16:06:13 +0800

    update navbar

--- final-project/js/popup-timer.js
+++ final-project/js/popup-timer.js
@@ -2,19 +2,19 @@ jQuery(document).ready(function($){
 
 	'use strict';
 
-	var ESC_KEY = 27;
+	var $ESC_KEY = 27;
 	
 	//close popup
-	$('.cd-popup').on('click', function(event){
-		if( $(event.target).is('.cd-close') || $(event.target).is('.cd-popup') ) {
+	$('.cd-popup-timer').on('click', function(event){
+		if( $(event.target).is('.cd-close') || $(event.target).is('.cd-popup-timer') ) {
 			event.preventDefault();
 			$(this).removeClass('is-visible');
 		}
 	});
 	//close popup when clicking the enter key
 	$(document).keyup(function(event){
-    	if(event.which==ESC_KEY) {	
-    		$('.cd-popup').removeClass('is-visible');
+    	if(event.which==$ESC_KEY) {	
+    		$('.cd-popup-timer').removeClass('is-visible');
 	    }
     });
 });
